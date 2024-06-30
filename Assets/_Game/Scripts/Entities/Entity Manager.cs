8a8c6a51cce28f15e4fcd4db62d9b88c40792d27
Max <max.nibler@gmail.com>
2024-06-30 23:47:06 +0900

    Reorganized the Directories

---
+++ Assets/_Game/Scripts/Entities/Entity Manager.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EntityManager : MonoBehaviour
+{
+    public static EntityManager Instance { get; private set; }
+    private Dictionary<PlayerAccess, Vector3> _playerPositions;
+    private Dictionary<EnemyAccess, Vector3> _enemyPositions;
+    private Dictionary<EnemyAccess, PlayerAccess> _targeting;
+
+    void Start()
+    {
+        if (Instance != null) Destroy(this);
+        Instance = this;
+        _playerPositions = new Dictionary<PlayerAccess, Vector3>();
+        _enemyPositions = new Dictionary<EnemyAccess, Vector3>();
+        _targeting = new Dictionary<EnemyAccess, PlayerAccess>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdatePosition(PlayerAccess playerPosition, Vector3 location)
+    {
+        _playerPositions[playerPosition] = location;
+    }
+
+    public void UpdatePosition(EnemyAccess enemyPosition, Vector3 location)
+    {
+        _enemyPositions[enemyPosition] = location;
+    }
+
+    public List<Vector3> GetPlayerPositions()
+    {
+        List<Vector3> playerPostions = new List<Vector3>();
+        foreach (Vector3 value in _playerPositions.Values)
+        {
+            playerPostions.Add(value);
+        }
+        return playerPostions;
+    }
+
+    public List<Vector3> GetEnemyPositions()
+    {
+        List<Vector3> enemyPositions = new List<Vector3>();
+        foreach (Vector3 value in _enemyPositions.Values)
+        {
+            enemyPositions.Add(value);
+        }
+        return enemyPositions;
+    }
+
+    public List<PlayerAccess> GetPlayers()
+    {
+        List<PlayerAccess> pas = new List<PlayerAccess>();
+        foreach (PlayerAccess pa in _playerPositions.Keys)
+        {
+            pas.Add(pa);
+        }
+        return pas;
+    }
+
+    public void RegisterTarget(EnemyAccess ea, PlayerAccess pa)
+    {
+        _targeting[ea] = pa;
+    }
+
+    public void ClearTarget(EnemyAccess ea)
+    {
+        _targeting.Remove(ea);
+    }
+
+    public Vector3 GetPath(EnemyAccess ea)
+    {
+        PlayerAccess pa = _targeting[ea];
+        EnemyAccess closest = getClosestEnemy(pa);
+        if (closest == ea) return pa.Position;
+        if (!ea.CanSee(closest)) return pa.Position;
+        return getIndirectDestination(ea.Position, pa.Position, closest.Position);
+    }
+
+    private EnemyAccess getClosestEnemy(PlayerAccess pa)
+    {
+        EnemyAccess closest = null;
+        foreach (EnemyAccess ea in _targeting.Keys)
+        {
+            if (_targeting[ea] == pa) 
+            {
+                if (closest == null) closest = ea;
+                closest = closerTargeting(closest, pa, ea);
+            }
+        }
+        if (closest == null) throw new Exception("No closes enemy found for this check");
+        return closest;
+    }
+
+    private EnemyAccess closerTargeting(EnemyAccess current, PlayerAccess pa, EnemyAccess checking)
+    {
+        float oldDistance = getDistance(pa, current);
+        float newDistance = getDistance(pa, checking);
+        return newDistance < oldDistance ? checking : current;
+    }
+
+    private Vector3 getIndirectDestination(Vector3 from, Vector3 to, Vector3 closerEnemy)
+    {
+        float opposite = Vector3.Distance(to, closerEnemy);
+        float hyp = Vector3.Distance(from, to);
+        float distance = (float) Math.Sqrt(hyp * hyp - opposite * opposite); 
+        float angle = (float) (Math.Asin(opposite / hyp) * 180 / Math.PI);
+        Vector3 toOther = closerEnemy - from;
+        Vector3 forwardDirection = (to - from).normalized;
+        float angleOfOtherEnemy = Vector3.SignedAngle(forwardDirection, toOther, Vector3.up);
+        int negative = angleOfOtherEnemy > 0 ? -1 : 1;
+        Vector3 turnedDirection = Quaternion.AngleAxis(angle * negative, Vector3.up) * forwardDirection;
+
+        return from + turnedDirection * distance;
+    }
+
+    private float getDistance(PlayerAccess pa, EnemyAccess ea)
+    {
+        return Vector3.Distance(ea.Position, pa.Position);
+    }
+}
