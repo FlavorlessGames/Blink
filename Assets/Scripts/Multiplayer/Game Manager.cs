0e79ea8e7a9f3517fe51e27b34f4382fa36c99d4
Max <max.nibler@gmail.com>
2024-06-30 12:32:42 +0900

    set spawn point based on an existing player prefab or allow it to be set to a specific game object

--- Assets/Scripts/Multiplayer/Game Manager.cs
+++ Assets/Scripts/Multiplayer/Game Manager.cs
@@ -1,12 +1,32 @@
 using Unity.Netcode;
 using UnityEngine;
+using System;
 
 public class GameManager : NetworkBehaviour {
     [SerializeField] private PlayerMovement _playerPrefab;
+    [SerializeField] private GameObject _spawnPoint;
 
     void Start()
     {
         if (!IsSpawned) InitSingleplayer(); // Todo: make this safer
+       
+    }
+
+    public void FindSpawnPoint()
+    {
+        UnityEngine.Object [] objs = FindObjectsOfType(typeof(PlayerMovement), true);
+        if (objs.Length < 1) throw new Exception("Player Spawn point not found or set");
+        PlayerMovement pm = (PlayerMovement) objs[0];
+        _spawnPoint = pm.gameObject;
+    }
+    
+    private void disablePlayerPrefabs()
+    {
+        PlayerMovement [] objs = (PlayerMovement []) FindObjectsOfType(typeof(PlayerMovement), true);
+        foreach (PlayerMovement pm in objs)
+        {
+            pm.gameObject.SetActive(false);
+        }
     }
 
     public void InitSingleplayer()
@@ -15,13 +35,24 @@ public class GameManager : NetworkBehaviour {
     }
 
     public override void OnNetworkSpawn() {
+        try
+        {
+            if (_spawnPoint.transform == null) FindSpawnPoint();
+        }
+        catch (UnassignedReferenceException)
+        {
+            Debug.Log("Spawn Point was not manually set. Searching for a player prefab in the scene");
+            FindSpawnPoint();
+        }
+        disablePlayerPrefabs();
         SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId);
     }   
 
     [ServerRpc(RequireOwnership = false)]
     private void SpawnPlayerServerRpc(ulong playerId) {
-        var spawn = Instantiate(_playerPrefab);
-        spawn.NetworkObject.SpawnWithOwnership(playerId);
+        Transform sp = _spawnPoint.transform;
+        GameObject spawn = (GameObject) Instantiate(_playerPrefab.gameObject, sp.position, sp.rotation);
+        spawn.GetComponent<NetworkObject>().SpawnWithOwnership(playerId);
     }
 
     public override void OnDestroy() {
